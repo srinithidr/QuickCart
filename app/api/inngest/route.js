c39e95fc502b77b748621bbbc65f86ad4f97d8ea
Srinidhi <srinithidr@gmail.com>
2026-04-17 10:52:34 +0530

    your message

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,19 @@
+import { serve } from "inngest/next";
+
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "../../config/inngest";
+
 export const runtime = "nodejs";
 
-export async function GET() {
-  return new Response("API working");
-}
-
-export async function POST() {
-  return new Response("API working");
-}
+export const { GET, POST } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
